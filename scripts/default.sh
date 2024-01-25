ac9f22c39874d5ab756a5a4fbd5960841079e1e2
sigreen-nokia <simon.green@nokia.com>
2024-01-25 21:47:20 +0000

    Added support for the webhook changes in 23.11, we support pre and post 23.11 now

--- scripts/default.sh
+++ scripts/default.sh
@@ -11,41 +11,48 @@
 #echo "DEBUG: Query parameter: Status=$Status" >> /tmp/gazoo-commands/message-decode
 #echo "DEBUG: Header parameter: user-agent=$user_agent" >> /tmp/gazoo-commands/message-decode
 #echo "DEBUG: JSON data block decoded:" >> /tmp/gazoo-commands/message-decode
+#echo "DEBUG: Payload Raw JSON" >> /tmp/gazoo-commands/message-decode
+#echo "$1" >> /tmp/gazoo-commands/message-decode
+#echo "DEBUG: Payload Formatted JSON" >> /tmp/gazoo-commands/message-decode
 #echo "$1" | jq . >> /tmp/gazoo-commands/message-decode
 
 #extract the event and the event id from the JSON data block
-#note we are dealing with a bug here, for the test message .status for everything else .Status
-STATUS=`sed -e 's/^"//' -e 's/"$//' <<< $(jq '.Status' <<< "$1")` 
+#note we are dealing with a bug here, for the test message we see .status for everything else .Status
+#We are also dealing with a stange from .Status to ."Event Status" in 23.11
+STATUS=`sed -e 's/^"//' -e 's/"$//' <<< $(jq '.Status, .status, ."Event Status"' <<< "$1") | grep -v null` 
 EVENTID=`sed -e 's/^"//' -e 's/"$//' <<< $(jq '."Event ID"' <<< "$1")` 
-#echo "DEBUG: STATUS is: $STATUS" >> /tmp/gazoo-commands/streamer
-#echo "DEBUG: EVENTID is: $EVENTID" >> /tmp/gazoo-commands/streamer
+MESSAGETYPE=`sed -e 's/^"//' -e 's/"$//' <<< $(jq '."Message type"' <<< "$1")`
+#echo "DEBUG: STATUS is: $STATUS" >> /tmp/gazoo-commands/message-decode
+#echo "DEBUG: EVENTID is: $EVENTID" >> /tmp/gazoo-commands/message-decode
+#echo "DEBUG: MESSAGETYPE is: $MESSAGETYPE" >> /tmp/gazoo-commands/message-decode
 
 
 #this block handles the event to speech
 #
 #Based on the event echo details into the shared file to play on the host
 #note here you could also use curl instead here, to send a rest api out to another platform rather than play sounds 
-#
+#We are dealing with pre 23.11 and post 23.11 API formats in here.
 case "$STATUS" in
   "ACTIVE")
-    echo "Defender Start event $EVENTID" >> /tmp/gazoo-commands/streamer
+    if [ "$MESSAGETYPE" = "Event started" ] || [ "$MESSAGETYPE" = "null" ]; then
+        echo "Defender Start event $EVENTID" >> /tmp/gazoo-commands/streamer
+    fi
     ;;
 
   "FINISHED")
-    echo "Defender Stop event $EVENTID" >> /tmp/gazoo-commands/streamer
+    if [ "$MESSAGETYPE" = "Event finished" ] || [ "$MESSAGETYPE" = "null" ]; then
+        echo "Defender Stop event $EVENTID" >> /tmp/gazoo-commands/streamer
+    fi
     ;;
 
+  "TEST")
+    echo "Defender Test event" >> /tmp/gazoo-commands/streamer
+    ;;
   *)
-    #test events uses .status not .Status, seems like a bug but lets handle that
-    if [ "$STATUS" = "null" ]; then
-        TESTSTATUS=`sed -e 's/^"//' -e 's/"$//' <<< $(jq '.status' <<< "$1")` 
-        #echo "DEBUG: TESTSTATUS is: $TESTSTATUS" >> /tmp/gazoo-commands/streamer
-        if [ "$TESTSTATUS" = "TEST" ]; then
-            echo "Defender Test event" >> /tmp/gazoo-commands/streamer
-        else
-            echo "Defender unrecognised event" >> /tmp/gazoo-commands/streamer
-        fi    
-    fi
+    echo "Defender unrecognised event" >> /tmp/gazoo-commands/message-decode
+    echo "DEBUG: STATUS is: $STATUS" >> /tmp/gazoo-commands/message-decode
+    echo "DEBUG: EVENTID is: $EVENTID" >> /tmp/gazoo-commands/message-decode
+    echo "DEBUG: MESSAGETYPE is: $MESSAGETYPE" >> /tmp/gazoo-commands/message-decode
     ;;
 esac
 
